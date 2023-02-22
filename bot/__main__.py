4bbbbbefc7c6e16f7c08aca7c0e2df31b1b18c76
Temirlan <temir8173>
2023-02-22 16:03:27 +0500

    fix

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,25 +1,22 @@
 import logging
 
 from aiogram import Bot
-from aiogram.contrib.middlewares.i18n import I18nMiddleware
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from aioredis import Redis
 from aiohttp import web
 
-from bot.config import TOKEN, SQLALCHEMY_ASYNC_DB_URI, redis_credentials, WORKDIR
+from bot.config import TOKEN, SQLALCHEMY_ASYNC_DB_URI, redis_credentials
 from bot.db import create_async_engine, get_session_maker
 
-logging.basicConfig(format=u'%(filename)+13s [ LINE:%(lineno)-4s] %(levelname)-8s [%(asctime)s] %(message)s',
-                    level=logging.DEBUG)
+# logging.basicConfig(format=u'%(filename)+13s [ LINE:%(lineno)-4s] %(levelname)-8s [%(asctime)s] %(message)s',
+#                     level=logging.DEBUG)
 
 async_engine = create_async_engine(SQLALCHEMY_ASYNC_DB_URI)
 session_maker = get_session_maker(async_engine)
 
 redis = Redis(**redis_credentials)
-
-# PROXY_URL = "http://37.53.103.4:3128"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=RedisStorage2(**redis_credentials))
@@ -42,11 +39,3 @@ async def on_shutdown(dispatcher: Dispatcher):
 if __name__ == '__main__':
     print('ok')
     executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
-
-
-
-# import redis
-#
-# r = redis.Redis(host='redis', port=6379, db=0)
-# r.set('foo', 'bar')
-# print(r.get('foo'))
